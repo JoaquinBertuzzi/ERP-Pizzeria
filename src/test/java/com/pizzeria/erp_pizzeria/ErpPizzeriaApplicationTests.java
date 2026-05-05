59089ee1b89f97781f87f4035747adce415e8839
SirOingcelot <joaquinbm1@gmail.com>
2026-05-06 01:03:41 +0200

    Ejercicio ERP+Springboot

---
+++ src/test/java/com/pizzeria/erp_pizzeria/ErpPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzeria.erp_pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
